9b924729a145aea058bd8649d2022327a54ffd63
peerapat1599 <pee1159@hotmail.com>
2019-10-02 19:20:28 +0700

    5.3

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -57,11 +57,9 @@ class GameFragment : Fragment() {
         viewModel.resetList()
         viewModel.nextWord()
         binding.gameViewModel = viewModel
+        binding.lifecycleOwner = this
         viewModel.score.observe(this, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
-        })
-        viewModel.word.observe(this, Observer { newWord ->
-            binding.wordText.text = newWord
         })
         viewModel.eventGameFinish.observe(this, Observer<Boolean> { hasFinished ->
             if (hasFinished) gameFinished()
